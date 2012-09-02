b5446393c0fe4802dedf7b40c7c1bcc20214c54b
Anders Karlsen <karlsenanders@gmail.com>
2012-09-02 13:53:14 +0200

    Straights

--- src/yatzeeclojure/core.clj
+++ src/yatzeeclojure/core.clj
@@ -29,8 +29,30 @@
 )
 
 (with-test 
-  (defn chance [dice]
-    (reduce + dice)
+  (defn chance [dices]
+    (reduce + dices)
     )
   (is (= 20 (chance [2 3 6 6 3])))
 )
+
+(with-test
+  (defn house [dices]
+    (if (= (set (vals (dice-map dices))) #{2 3}) (reduce + dices) 0)
+    )
+  (is (= 0 (house [1 2 4 4 5])))
+  (is (= 16 (house [2 2 2 5 5])))
+  )
+
+(with-test
+  (defn low-straight [dices]
+    (if (= (set dices) #{1 2 3 4 5}) 15 0))
+  (is (= 0 (low-straight [3 4 5 3 1])))
+  (is (= 15 (low-straight [5 4 2 3 1])))
+  )
+
+(with-test
+  (defn high-straight [dices]
+    (if (= (set dices) #{2 3 4 5 6}) 20 0))
+  (is (= 0 (high-straight [3 4 5 3 1])))
+  (is (= 20 (high-straight [5 4 2 3 6])))
+  )
